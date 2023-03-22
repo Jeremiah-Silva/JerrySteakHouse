f921c1882b6df105a2a6d259a0f7f8fb94cafc8c
Jeremiah Silva <jeremiah.ca9@gmail.com>
2023-03-22 15:39:45 -0600

    Improved CartProvider

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -52,9 +52,12 @@ const cartReducer =(state, action) => {
         };
     }
     
+ if (action.type === 'CLEAR') {
+    return defaultCartState;
+ }
+
     return defaultCartState;
 };
-
 
 const CartProvider = props => {
     const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
@@ -67,11 +70,16 @@ const CartProvider = props => {
         dispatchCartAction({type: 'REMOVE', id: id });
     };
 
+    const clearCartHandler = () => {
+        dispatchCartAction({type: 'CLEAR'})
+    };  
+
     const cartContext = {
         items: cartState.items,
         totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
         removeItem: removeItemFromCartHandler,
+        clearCart: clearCartHandler
     };
 
     return (
